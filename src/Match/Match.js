670fa7129abf7f77bfaa333221e9d4f753ffe78e
Eric Solner <ericsolner2018@u.northwestern.edu>
2018-05-02 21:02:31 -0500

    made very basic grid layout template for the match page

--- src/Match/Match.js
+++ src/Match/Match.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Icon } from 'native-base';
-export default class IconExample extends Component {
+import { Container, Header, Content } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+export default class LayoutExample extends Component {
   render() {
     return (
       <Container>
         <Header />
         <Content>
-          <Icon name='home' />
-          <Icon ios='ios-menu' android="md-menu" style={{fontSize: 20, color: 'red'}}/>
-          <Icon type="FontAwesome" name="home" />
-          <Icon name='home' />
-          <Icon ios='ios-menu' android="md-menu" style={{fontSize: 20, color: 'red'}}/>
-          <Icon type="FontAwesome" name="home" />
-          <Icon name='home' />
-          <Icon ios='ios-menu' android="md-menu" style={{fontSize: 20, color: 'red'}}/>
-          <Icon type="FontAwesome" name="home" />
-          <Icon name='home' />
-          <Icon ios='ios-menu' android="md-menu" style={{fontSize: 20, color: 'red'}}/>
-          <Icon type="FontAwesome" name="home" />
-          
-        </Content>
+          <Grid>
+            <Col style={{ backgroundColor: '#635DB7', width: 200}}>
+            <Row style={{ backgroundColor: '#00CE9F', height: 200, width: 200} } ></Row>
+            <Row style={{ backgroundColor: '#635DB7', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#00CE9F', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#635DB7', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#00CE9F', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#635DB7', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#00CE9F', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#635DB7', height: 200, width: 200}}></Row>
+            </Col>
+
+            <Col style={{ backgroundColor: '#00CE9F',  width: 200}}>
+            <Row style={{ backgroundColor: '#635DB7', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#00CE9F', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#635DB7', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#00CE9F', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#635DB7', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#00CE9F', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#635DB7', height: 200, width: 200}}></Row>
+            <Row style={{ backgroundColor: '#00CE9F', height: 200, width: 200}}></Row>
+            </Col>
+          </Grid>
+        </Content>Row
       </Container>
     );
   }
