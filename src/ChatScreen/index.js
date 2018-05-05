80ead4d58ff62b1ab1c801bf90f5606a83912bfe
zhemaitite-anastasiia <zhemaititea@gmail.com>
2018-05-04 21:11:54 -0500

    Couple chats created and styled

--- src/ChatScreen/index.js
+++ src/ChatScreen/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
-import LucyChat from "./LucyChat.js";
-import JadeChat from "./JadeChat.js";
+import EnasChat from "./EnasChat.js";
+import EricChat from "./EricChat.js";
+import AnastasiiaChat from "./AnastasiiaChat.js";
+import EllieChat from "./EllieChat.js";
+import MattChat from "./MattChat.js";
+import Conversations from "./Conversations.js";
 import NineChat from "./NineChat.js";
+import NewChat from "./NewChat.js";
 import testChat from "./testChat.js";
 import { TabNavigator } from "react-navigation";
 import {
@@ -11,13 +16,19 @@ import {
   Item,
   Footer,
   FooterTab,
-  Label
+  Label,
+  StyleProvider
 } from "native-base";
 export default (MainScreenNavigator = TabNavigator(
   {
-    LucyChat: { screen: props => <LucyChat {...props} /> },
-    JadeChat: { screen: props => <JadeChat {...props} /> },
-    NineChat: { screen: props => <NineChat {...props} /> }
+    EnasChat: { screen: props => <EnasChat {...props} /> },
+    EricChat: { screen: props => <EricChat {...props} /> },
+    AnastasiiaChat: { screen: props => <AnastasiiaChat {...props} /> },
+    EllieChat: { screen: props => <EllieChat {...props} /> },
+    MattChat: { screen: props => <MattChat {...props} /> },
+    Conversations: { screen: props => <Conversations {...props} /> },
+    NewChat: { screen: props => <NewChat {...props} /> },
+    // NineChat: { screen: props => <NineChat {...props} /> }
   },
   {
     tabBarPosition: "bottom",
@@ -25,30 +36,50 @@ export default (MainScreenNavigator = TabNavigator(
       return (
         <Footer>
           <FooterTab>
-            <Button
+          <Button style={{width:"100%"}}
               vertical
               active={props.navigationState.index === 0}
-              onPress={() => props.navigation.navigate("LucyChat")}
+              onPress={() => props.navigation.navigate("NewChat")}
             >
-              <Icon name="bowtie" />
-              <Text>New Chat</Text>
+              <Icon style={{marginLeft:25}} name="bowtie" />
+              <Text style={{
+            
+            fontSize:15,
+            marginLeft:5
+          }}>NewChat</Text>
             </Button>
-            <Button
+          <Button style={{
+            marginLeft: 10,
+            width:"100%",
+           
+          }}
               vertical
               active={props.navigationState.index === 1}
-              onPress={() => props.navigation.navigate("JadeChat")}
+              onPress={() => props.navigation.navigate("Conversations")}
             >
               <Icon name="briefcase" />
-              <Text>Conversations</Text>
+              <Text style={{
+            width: "180%",
+            marginLeft:170,
+           
+            fontSize:15
+          }}>Conversations</Text>
             </Button>
-            <Button
+            
+            
+            {/* <Button style={{
+              width:"50%",
+            marginRight: "10%",
+           
+          }}
+
               vertical
               active={props.navigationState.index === 2}
               onPress={() => props.navigation.navigate("NineChat")}
-            >
-              <Icon name="headset" />
-              <Text>Settings</Text>
-            </Button>
+            > */}
+              {/* <Icon name="headset" />
+              <Text>Settings</Text> */}
+            {/* </Button>  */}
           </FooterTab>
         </Footer>
       );
